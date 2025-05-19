574ce1abe46225285bcbbf1fc6865bafe2a699f2
ra1n6ow <jeffduuu@gmail.com>
2025-05-19 23:28:58 +0800

    feature: version flag

--- cmd/fg-apiserver/app/server.go
+++ cmd/fg-apiserver/app/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/spf13/viper"
 
 	"github.com/ra1n6ow/fastgo/cmd/fg-apiserver/app/options"
+	"github.com/ra1n6ow/fastgo/pkg/version"
 )
 
 var configFile string // 配置文件路径
@@ -39,11 +40,17 @@ func NewFastGOCommand() *cobra.Command {
 	// 推荐使用配置文件来配置应用，便于管理配置项
 	cmd.PersistentFlags().StringVarP(&configFile, "config", "c", filePath(), "Path to the fg-apiserver configuration file.")
 
+	// 添加 --version 标志
+	version.AddFlags(cmd.PersistentFlags())
+
 	return cmd
 }
 
 // 主运行逻辑，负责初始化日志，解析配置，校验选项并启动服务器
 func run(opts *options.ServerOptions) error {
+	// 如果传入 --version，则打印版本信息并退出
+	version.PrintAndExitIfRequested()
+
 	// 将 viper 中的配置解析到选项 opts 变量中.
 	if err := viper.Unmarshal(opts); err != nil {
 		return err
